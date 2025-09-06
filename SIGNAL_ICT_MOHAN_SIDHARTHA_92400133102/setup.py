0607acb8c8e8b516c9f6e0d98e5ab014a9f1db6b
sidhartha914 <uurssiddu@gmail.com>
2025-09-06 18:36:00 +0530

    Add files via upload

---
+++ SIGNAL_ICT_MOHAN_SIDHARTHA_92400133102/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="signal_ICT_MOHAN_SIDHARTHA_92400133102",   # <-- Replace with your actual student name & roll no
+    version="0.1",
+    author="L.Mohan Sidhartha",
+    author_email="mohansidhartha.lingampalli129237@marwadiuniversity.ac.in",
+    description="A Python package for generating and working with basic signals",
+    long_description_content_type="text/markdown",
+    url="https://test.pypi.org/project/signal_ICT_Rottela_Rakesh_92400133094/",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "numpy",
+        "matplotlib"
+    ]
+)
